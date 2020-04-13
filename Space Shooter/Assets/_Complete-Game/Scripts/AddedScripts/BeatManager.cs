4c8392cb216a16380d58f76e2c3512739bc36613
Arthur Muddiman <tidus9000@gmail.com>
2020-04-13 12:53:31 +0100

    implemented basic rhythm mechanic into space shooter

---
+++ Space Shooter/Assets/_Complete-Game/Scripts/AddedScripts/BeatManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatManager : MonoBehaviour
+{
+    float m_BPM = 0;
+    [SerializeField]MusicManager m_musicMan;
+    GameManager gm;
+    Spawner m_sp;
+    int currentBeat = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_sp = GetComponent<Spawner>();
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        m_musicMan = GameObject.Find("MusicManager").GetComponent<MusicManager>();
+        //m_BPM = m_musicMan.timelineInfo.tempo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentBeat != m_musicMan.timelineInfo.currentBeat)
+        {
+            currentBeat = m_musicMan.timelineInfo.currentBeat;
+            //spawn a beat marker
+            m_sp.Spawn();
+            gm.m_timeToNextBeat = 60 / m_BPM;
+        }
+
+        if (m_BPM != m_musicMan.timelineInfo.tempo)
+        {
+            m_BPM = m_musicMan.timelineInfo.tempo;
+        }
+
+        gm.m_timeToNextBeat -= Time.deltaTime;
+    }
+}
---
+++ Space Shooter/Assets/_Complete-Game/Scripts/AddedScripts/BeatMarker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatMarker : MonoBehaviour
+{
+    [SerializeField]float m_speed;
+    [SerializeField]Vector2 m_target;
+    MusicManager m_music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = GameObject.Find("Canvas").transform;
+
+        //when beat marker spawns, we want to set its speed so that it goes to the target after one bar
+        m_target = GameObject.Find("BeatTargetPoint").transform.position;
+        m_music = GameObject.Find("MusicManager").GetComponent<MusicManager>();
+        float beatsPerBar = m_music.timelineInfo.beatsPerBar;
+        float timePerBeat = 60 / m_music.timelineInfo.tempo;
+        float travelTime = timePerBeat * beatsPerBar;
+
+        //speed = distance / time
+        float distance = Vector2.Distance(transform.position, m_target);
+        m_speed = distance / travelTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newpos = Vector2.MoveTowards(transform.position, m_target, m_speed * Time.deltaTime);
+
+        transform.position = newpos;
+
+        if (transform.position.x == m_target.x && transform.position.y == m_target.y)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
